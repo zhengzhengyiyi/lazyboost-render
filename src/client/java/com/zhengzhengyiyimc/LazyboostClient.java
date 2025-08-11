1673c424d04fe20bfc94ef5872a4032a4ad9fbd2
zhengzhengyiyi <zhengzhichen44@163.com>
2025-08-10 20:00:34 -0400

    v1.0.1

--- src/client/java/com/zhengzhengyiyimc/LazyboostClient.java
+++ src/client/java/com/zhengzhengyiyimc/LazyboostClient.java
@@ -4,15 +4,20 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.ActionResult;
 import org.lwjgl.glfw.GLFW;
 
 public class LazyboostClient implements ClientModInitializer {
     private long windowHandle = -1L;
     private boolean isFpsReduced = false;
-    private int originalFpsLimit = 120; 
+    private int originalFpsLimit = 120;
+
+	private KeyBinding openConfigKey;
 
     @Override
     public void onInitializeClient() {
@@ -21,6 +26,13 @@ public class LazyboostClient implements ClientModInitializer {
         AutoConfig.getConfigHolder(ModConfig.class).registerSaveListener((manager, data) -> {
             return ActionResult.SUCCESS;
         });
+
+		openConfigKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.lazyboost.opengui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_UNKNOWN,
+                "key.lazyboost.key"
+        ));
 
         ClientTickEvents.START_CLIENT_TICK.register(mc -> {
             if (windowHandle == -1L && mc.getWindow() != null) {
@@ -32,6 +44,10 @@ public class LazyboostClient implements ClientModInitializer {
     }
 
     private void onClientTick(MinecraftClient client) {
+		if (openConfigKey.isPressed()) {
+			client.setScreen(AutoConfig.getConfigScreen(ModConfig.class, client.currentScreen).get());
+		}
+
 		if (!AutoConfig.getConfigHolder(ModConfig.class).getConfig().enableAutoFpsReduction) return;
 
         if (client.player == null || client.world == null || windowHandle == -1L) {
